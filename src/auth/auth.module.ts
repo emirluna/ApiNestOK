3cafda8560a53f82c1e8c12c08f2575c197c5d08
emirluna <luis.emir.lm@gmail.com>
2019-07-01 06:02:14 -0500

    Primer version de login correcta

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from '../users/users.module';
+import { PassportModule } from '@nestjs/passport';
+
+@Module({
+
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }),
+    JwtModule.register({
+      secretOrPrivateKey: 'thisismykickasssecretthatiwilltotallychangelater',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+    UsersModule
+  ],
+
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy]
+})
+export class AuthModule {}
